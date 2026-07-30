994830f6546b171d257df0350d2977743fe9af56
krutthiikaaa <kruthikakonni536@gmail.com>
2026-07-30 12:22:02 +0400

    Remove Faculty portal frontend and routes

--- frontend/src/app/faculty/layout.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { FacultyLayout } from '@/modules/faculty/components/layout/FacultyLayout';
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <FacultyLayout>{children}</FacultyLayout>;
-}
